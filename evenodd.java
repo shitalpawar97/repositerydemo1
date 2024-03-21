cecb032967ef737c5141d55de51aaa90a01474e5
shitalpawar97 <68606550+shitalpawar97@users.noreply.github.com>
2024-03-21 12:34:59 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+package Examples;
+
+public class evenodd {
+	public static void main(String[] args) {
+		int no=11;
+		if(no%2==0){
+			System.out.println("Number is even...");
+		}
+		else
+		{
+			System.out.println("Number is odd...");
+		}
+	}
+
+}
